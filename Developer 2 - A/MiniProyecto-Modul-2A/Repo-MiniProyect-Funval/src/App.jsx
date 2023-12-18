a36ebbcaa77c53710a227f841a09e86fb7574824
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2023-12-18 18:17:39 -0500

    RESTful_api_registro

--- Developer 2 - A/MiniProyecto-Modul-2A/Repo-MiniProyect-Funval/src/App.jsx
+++ Developer 2 - A/MiniProyecto-Modul-2A/Repo-MiniProyect-Funval/src/App.jsx
@@ -1,40 +1,93 @@
-import { useEffect, useState } from "react";
-import Card from "./components/Card";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import Card from './components/Card';
+import './App.css';
 
 function App() {
-  // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
   const [data, setData] = useState([]);
 
-  // Función para traer los datos de "stays.json".
-  const getData = async () => {
-    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
-    try {
-      const res = await fetch("stays.json");
-      const resJson = await res.json();
-      // Aquí guardamos los datos de "stays.json" en la variable data.
-      setData(resJson);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
   useEffect(() => {
-     getData();
+    const fetchData = async () => {
+      try {
+        const res = await fetch("stays.json");
+        const resJson = await res.json();
+        setData(resJson);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  // Puedes ver la variable data en consola.
-  //console.log(data);
+  const itemsPerPage = 3;
+
+  const firstRowData = data.slice(0, 7); // Del índice 0 al 6
+  const secondRowData = data.slice(7, 14); // Del índice 7 al 14
+
+  const totalPagesFirstRow = Math.ceil(firstRowData.length / itemsPerPage);
+  const totalPagesSecondRow = Math.ceil(secondRowData.length / itemsPerPage);
+
+  const [currentPage1, setCurrentPage1] = useState(0);
+  const [currentPage2, setCurrentPage2] = useState(0);
+
+  const startIndex1 = currentPage1 * itemsPerPage;
+  const endIndex1 = startIndex1 + itemsPerPage;
+  const visibleData1 = firstRowData.slice(startIndex1, endIndex1);
+
+  const startIndex2 = currentPage2 * itemsPerPage;
+  const endIndex2 = startIndex2 + itemsPerPage;
+  const visibleData2 = secondRowData.slice(startIndex2, endIndex2);
+
+  const handlePrevClick1 = () => {
+    setCurrentPage1((prev) => Math.max(prev - 1, 0));
+  };
+
+  const handleNextClick1 = () => {
+    setCurrentPage1((prev) => Math.min(prev + 1, totalPagesFirstRow - 1));
+  };
+
+  const handlePrevClick2 = () => {
+    setCurrentPage2((prev) => Math.max(prev - 1, 0));
+  };
+
+  const handleNextClick2 = () => {
+    setCurrentPage2((prev) => Math.min(prev + 1, totalPagesSecondRow - 1));
+  };
+
   return (
-    <>
-    {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
-     <div>
-     {data.map((el, i) => {
-      return <Card key={i} photo={el.photo} title={el.title} />
-    })}
+    <div className='card-aptos'>
+      <div className="listas-orientadas">
+        <div className='btn-bf'>
+        <button onClick={handlePrevClick1} disabled={currentPage1 === 0}>&lt;
+          </button>
+        </div>
+        {visibleData1.map((el, i) => (
+          <Card key={i} photo={el.photo} title={el.title} type={el.type} beds={el.beds} superHost={el.superHost} rating={el.rating}/>
+        ))}
+        {/* Botones para la primera fila */}
+        <div className='btn-aft'>
+          <button onClick={handleNextClick1} disabled={currentPage1 === totalPagesFirstRow - 1}>
+          &gt;
+          </button>
+        </div>
       </div>
-    </>
+      <div className="listas-orientadas">
+        <div className='btn-bf'>
+        <button onClick={handlePrevClick2} disabled={currentPage2 === 0}>
+        &lt;
+          </button>
+        </div>
+        {visibleData2.map((el, i) => (
+          <Card key={i} photo={el.photo} title={el.title} type={el.type} beds={el.beds} superHost={el.superHost} rating={el.rating}/>
+        ))}
+        {/* Botones para la segunda fila */}
+        <div className='btn-aft'>
+          <button onClick={handleNextClick2} disabled={currentPage2 === totalPagesSecondRow - 1}>
+          &gt;
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
